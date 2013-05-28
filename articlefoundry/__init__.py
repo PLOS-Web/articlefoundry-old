01a2cf5d2b94351c011c39d11addb4c3dbe8c763
Jack LaBarba <brakit@gmail.com>
2013-05-28 11:06:07 -0700

    Adds __init__

---
+++ articlefoundry/__init__.py
@@ -0,0 +1,7 @@
+__title__ = 'articlefoundry'
+__version__ = '0.1.0'
+__author__ = 'John LaBarba'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2013 John LaBarba'
+
+from .file_utils import *
